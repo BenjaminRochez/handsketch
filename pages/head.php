75000716a953f5118fdba656afc1d58573dbaa8c
Benjamin Rochez <squiweb@gmail.com>
2016-11-01 11:28:43 +0100

    refactoring BIG UPDATE

--- pages/head.php
+++ pages/head.php
@@ -21,7 +21,7 @@
   <meta name="twitter:image:src" content="http://diplostudio.be/assets/images/facebook.jpg"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
   <link rel="shortcut icon" href="favicon.ico"/>
-  <link href="main.css" rel="stylesheet" />
+  <link href="style.css" rel="stylesheet" />
   <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" />
   <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
