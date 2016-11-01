b89fc53c2b185cdd4b47edf2ec5718dad48ff467
Antony Paquay <antony.paquay@gmail.com>
2016-11-01 12:43:17 +0100

    Updated index.php

--- index.php
+++ index.php
@@ -3,25 +3,11 @@
 <body id="hovertone">
 
 <header>
-  <h1></h1>
+  <h1>We make special effects in the real life</h1>
+  <!-- Claque background -->
   <div>
-    <nav>
-      <h2>Hovertone</h2>
-      <ul>
-        <li>
-          <a href="">About</a>
-        </li>
-        <li>
-          <a href="">Product</a>
-        </li>
-        <li>
-          <a href="">Services</a>
-        </li>
-        <li>
-          <a href="">Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <?php include 'pages/nav.php' ?>
+    <!-- social -->
     <div class="social__bar">
       <ul class="social__bar__ul">
         <li class="social__bar__ul__li">
@@ -35,17 +21,124 @@
         </li>
       </ul>
     </div>
+    <!-- arrow anime -->
+    <img src="#" alt="#"/>
   </div>
 </header>
-    <!--  HAVE FUN :) -->
-    <!-- <footer id="contact">
-      <h3>
-        Hovertone
-      </h3>
-      <p>
-        Made with humans by diplostudio
-      </p>
-      <a href="#">Contact us</a>
-      <img src="assets/images/logo_handsketch.png" data-rjs="2" alt="logo handsketch" />
-    </footer> -->
+
+
+<!-- SECTION PRODUCTS -->
+<section>
+  <h2>Products</h2>
+  <!-- blockquote ?  or not -->
+  <h3>"We make special effects in the real life, creative experiences using technologies"</h3>
+  <!-- products list -->
+  <ul>
+    <li>
+      <a href="#">
+        <h4>Handsketch</h4>
+        <p>
+          Voice as an instrument
+        </p>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <h4>Handsketch</h4>
+        <p>
+          Voice as an instrument
+        </p>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <h4>Handsketch</h4>
+        <p>
+          Voice as an instrument
+        </p>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <h4>Handsketch</h4>
+        <p>
+          Voice as an instrument
+        </p>
+      </a>
+    </li>
+  </ul>
+</section>
+
+
+<!-- SECTION SERVICES -->
+<section>
+  <h2>Services</h2>
+  <!-- blockquote ?  or not -->
+  <h3>"Whether you want to organize a class about creative coding, a pratical hands-on workshop on motion and interraction"</h3>
+  <!-- products list -->
+  <ul>
+    <li>
+      <a href="#">
+        <h4>Motion capture</h4>
+        <p>
+          Voice as an instrument
+        </p>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <h4>Handsketch</h4>
+        <p>
+          Voice as an instrument
+        </p>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <h4>Handsketch</h4>
+        <p>
+          Voice as an instrument
+        </p>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <h4>Custom projects</h4>
+        <p>
+          Lets move to the next step together.
+        </p>
+      </a>
+    </li>
+  </ul>
+
+  <a href="#">Ask us for more information</a>
+</section>
+
+
+<!-- SECTION ABOUT -->
+<section>
+  <h2>About</h2>
+  <!-- TEAM -->
+  <div>
+    <img src="#" alt="" />
+    <p>
+      Hi world, we are Hovertone. We are a new startup in motion and music technologies. We aim at designing new musical experiences, creating new apps and new instruments like the HandSketch.
+    </p>
+  </div>
+  <div>
+    <img src="#" alt="" />
+    <p>
+      Hovertone is a combination of different technological expertises and high-tech platforms, serving these new interactive experiences, serving the emotion of the visitor, performer, creator, creative.
+    </p>
+  </div>
+</section>
+
+<!-- FOOTER -->
+<footer>
+  <h2>hovertone</h2>
+  <p>
+
+  </p>
+</footer>
+
 <?php include 'pages/end.php' ?>
