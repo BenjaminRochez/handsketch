0f6fe4ad94aa0c4a8585be9fbec72f597f028e42
Benjamin Rochez <squiweb@gmail.com>
2016-11-01 14:56:36 +0100

    theming drawingsounds

--- drawingsound.php
+++ drawingsound.php
@@ -6,8 +6,8 @@
     <h1></h1>
     <?php include 'pags/nav.php' ?>
   </header>
-  <section class="casestudy">
-    <h3 class="casestudy__title">Drawing sound</h3>
+  <section class="casestudy ">
+    <h2 class="casestudy__title">Drawing <br>sounds</h2>
     <p class="casestudy__description">
       Creating music is often considered difficult and limited to achieved
       musicians. With Drawing Sounds workshops, we want to demystify music
@@ -17,14 +17,14 @@
     <img src="assets/images/drawingsound.png" alt="" />
 
     <div class="casestudy__questions">
-      <h4 class="casestudy__questions__title">What?</h4>
-      <p>
+      <h3 class="casestudy__questions__title">What?</h4>
+      <p class="casestudy__questions__p">
         Creating music is often considered difficult and limited to achieved
         musicians. With Drawing Sounds workshops, we want to demystify music
       </p>
 
-      <h4 class="casestudy__questions__title">Do?</h4>
-      <p>
+      <h3 class="casestudy__questions__title">Do?</h4>
+      <p class="casestudy__questions__p">
         Creating music is often considered difficult and limited to achieved
         musicians. With Drawing Sounds workshops, we want to demystify music
       </p>
