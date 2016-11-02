bcde90e290112cc4ad873c2705ad21ebcd3bd8e0
Antony Paquay <antony.paquay@gmail.com>
2016-11-02 16:21:52 +0100

    Updated footer mobile first

--- index.php
+++ index.php
@@ -134,11 +134,6 @@
     </div>
 
     <!-- FOOTER -->
-    <footer>
-      <h2>hovertone</h2>
-      <p>
-
-      </p>
-    </footer>
+    <?php include 'pages/footer.php' ?>
 
     <?php include 'pages/end.php' ?>
--- pages/footer.php
+++ pages/footer.php
@@ -3,47 +3,47 @@
     <h2 class="redirection__h2">Hovertone</h2>
 
     <!-- PRODUCTS -->
-    <ul class="list__products">
-      <h3 class="list__products__h3">Products</h3>
-      <li class="list__products__li">
-        <a class="list__products__li__a" href="">Handsketch</a>
+    <ul class="list__title">
+      <h3 class="list__title__h3">Products</h3>
+      <li class="list__title__li">
+        <a class="list__title__li__a" href="">Handsketch</a>
       </li>
-      <li class="list__products__li">
-        <a class="list__products__li__a" href="">Drawing Sound</a>
+      <li class="list__title__li">
+        <a class="list__title__li__a" href="">Drawing Sound</a>
       </li>
-      <li class="list__products__li">
-        <a class="list__products__li__a" href="">Interactive Wall</a>
+      <li class="list__title__li">
+        <a class="list__title__li__a" href="">Interactive Wall</a>
       </li>
-      <li class="list__products__li">
-        <a class="list__products__li__a" href="">Choir Mob</a>
+      <li class="list__title__li">
+        <a class="list__title__li__a" href="">Choir Mob</a>
       </li>
     </ul>
 
     <!-- SERVICES -->
-    <ul class="list__services">
-      <h3 class="list__services__h3">Services</h3>
-      <li class="list__services__li">
-        <a class="list__services__li__a" href="">Motion Capture</a>
+    <ul class="list__title">
+      <h3 class="list__title__h3">Services</h3>
+      <li class="list__title__li">
+        <a class="list__title__li__a" href="">Motion Capture</a>
       </li>
-      <li class="list__services__li">
-        <a class="list__services__li__a" href="">Performances</a>
+      <li class="list__title__li">
+        <a class="list__title__li__a" href="">Performances</a>
       </li>
-      <li class="list__services__li">
-        <a class="list__services__li__a" href="">WorkShops</a>
+      <li class="list__title__li">
+        <a class="list__title__li__a" href="">WorkShops</a>
       </li>
-      <li class="list__services__li">
-        <a class="list__services__li__a" href="">Custom Project</a>
+      <li class="list__title__li">
+        <a class="list__title__li__a" href="">Custom Project</a>
       </li>
     </ul>
 
     <!-- CONTACT -->
-    <ul class="list__info">
-      <h3>Services</h3>
-      <li class="list__info__li">
-        <a class="list__info__li__a" href="mailto:info@hovertone.com">info@hovertone.com</a>
+    <ul class="list__title">
+      <h3 class="list__title__h3">Contact</h3>
+      <li class="list__title__li">
+        <a class="list__title__li__a" href="mailto:info@hovertone.com">info@hovertone.com</a>
       </li>
-      <li class="list__info__li">
-        <small class="list__info__li__small">4 rue des soeurs noires 7000 Mons Belgium</small>
+      <li class="list__title__li">
+        <small class="list__title__li__small">4 rue des soeurs noires 7000 Mons Belgium</small>
       </li>
     </ul>
   </div>
@@ -52,8 +52,8 @@
       Copyright Hovertone SPRL 2016 - TVA : BE0644610629
     </p>
     <div class="blackbar__box">
-        <small class="blackbar__box__small">Made with love by</small>
-        <img class="blackbar__box__img" src="assets/images/#" alt="logo DiploStudio"/>
+        <small class="blackbar__box__p">Made with love by <span>DiploStudio</span></small>
+
     </div>
   </div>
 </footer>
