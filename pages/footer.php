18471fe55f21f7d606b22b6294e457d12a742964
Antony Paquay <antony.paquay@gmail.com>
2016-11-02 15:18:24 +0100

    Add footer.php

---
+++ pages/footer.php
@@ -0,0 +1,59 @@
+<footer>
+  <div class="redirection">
+    <h2 class="redirection__h2">Hovertone</h2>
+
+    <!-- PRODUCTS -->
+    <ul class="list__products">
+      <h3 class="list__products__h3">Products</h3>
+      <li class="list__products__li">
+        <a class="list__products__li__a" href="">Handsketch</a>
+      </li>
+      <li class="list__products__li">
+        <a class="list__products__li__a" href="">Drawing Sound</a>
+      </li>
+      <li class="list__products__li">
+        <a class="list__products__li__a" href="">Interactive Wall</a>
+      </li>
+      <li class="list__products__li">
+        <a class="list__products__li__a" href="">Choir Mob</a>
+      </li>
+    </ul>
+
+    <!-- SERVICES -->
+    <ul class="list__services">
+      <h3 class="list__services__h3">Services</h3>
+      <li class="list__services__li">
+        <a class="list__services__li__a" href="">Motion Capture</a>
+      </li>
+      <li class="list__services__li">
+        <a class="list__services__li__a" href="">Performances</a>
+      </li>
+      <li class="list__services__li">
+        <a class="list__services__li__a" href="">WorkShops</a>
+      </li>
+      <li class="list__services__li">
+        <a class="list__services__li__a" href="">Custom Project</a>
+      </li>
+    </ul>
+
+    <!-- CONTACT -->
+    <ul class="list__info">
+      <h3>Services</h3>
+      <li class="list__info__li">
+        <a class="list__info__li__a" href="mailto:info@hovertone.com">info@hovertone.com</a>
+      </li>
+      <li class="list__info__li">
+        <small class="list__info__li__small">4 rue des soeurs noires 7000 Mons Belgium</small>
+      </li>
+    </ul>
+  </div>
+  <div class="blackbar">
+    <p class="blackbar__p">
+      Copyright Hovertone SPRL 2016 - TVA : BE0644610629
+    </p>
+    <div class="blackbar__box">
+        <small class="blackbar__box__small">Made with love by</small>
+        <img class="blackbar__box__img" src="assets/images/#" alt="logo DiploStudio"/>
+    </div>
+  </div>
+</footer>
